92b65e9b9ff53bdb4dae9b7ab5db9649e96cdd89
Muhammad Kamal <muh.kmal@gmail.com>
2018-04-20 22:54:19 +0700

    push project init

---
+++ src/main/java/com/siperpus/SiperpusTa4Application.java
@@ -0,0 +1,12 @@
+package com.siperpus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiperpusTa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiperpusTa4Application.class, args);
+	}
+}
