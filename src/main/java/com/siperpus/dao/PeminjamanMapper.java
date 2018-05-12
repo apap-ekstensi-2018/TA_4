922877d6d12646276798e67c0cfb17671b8b1e60
Muhammad Kamal <muh.kmal@gmail.com>
2018-05-12 22:53:40 +0700

    fixes conflict peminjaman mapper

--- src/main/java/com/siperpus/dao/PeminjamanMapper.java
+++ src/main/java/com/siperpus/dao/PeminjamanMapper.java
@@ -10,17 +10,8 @@ import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
-
-<<<<<<< HEAD
+ 
 import com.siperpus.model.PeminjamanModel;
-
-=======
-import com.siperpus.model.LiteratureModel;
-import com.siperpus.model.PeminjamanModel;
-
-import com.siperpus.model.LiteratureModel;
-
->>>>>>> f6c0cd2b75d131f70c7ec6c5069e37bdd41d857e
 @Mapper
 public interface PeminjamanMapper {
 	@Insert ("INSERT INTO peminjaman_literatur (judul, penulis, penerbit,jenis_literatur, jumlah) VALUES (#{judul}, #{penulis}, #{penerbit}, #{jenis_literatur}, #{jumlah})")
@@ -35,22 +26,10 @@ public interface PeminjamanMapper {
  @Select("SELECT * FROM peminjaman_literatur")
  List<PeminjamanModel> selectAllPeminjaman();
 
-@Select ("SELECT * FROM peminjaman_literatur WHERE id = #{id}")
-	@Results (value= {
-		@Result(property="id", column="id"),
-		@Result(property="judul",column="judul"),
-		@Result(property="penulis",column="penulis"),
-		@Result(property="penerbit",column="penerbit"),
-		@Result(property="jenis_literatur",column="jenis_literatur"),
-		@Result(property="jumlah",column="jumlah")})
-<<<<<<< HEAD
-	PeminjamanModel selectPeminjaman (@Param("id") Integer id); 
-=======
-	LiteratureModel selectLiterature (@Param("id") Integer id); 
+//@Select ("SELECT * FROM peminjaman_literatur WHERE id = #{id}")
 
 @Select("select id, id_literatur, username_peminjam, tanggal_peminjaman, tanggal_pengembalian, id_surat, status_peminjaman from peminjaman_literatur where id = #{id}")
-PeminjamanModel selectPeminjaman (@Param("id") Integer id);
->>>>>>> f6c0cd2b75d131f70c7ec6c5069e37bdd41d857e
+	PeminjamanModel selectPeminjaman (@Param("id") Integer id);  
 
 
 }
